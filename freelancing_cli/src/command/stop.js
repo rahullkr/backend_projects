0a5f9ef45bdc9c908a6d2e6b693684a8b4afda49
unknown <rk7318323176@gmail.com>
2024-07-08 12:36:52 +0530

    error handling done

--- freelancing_cli/src/command/stop.js
+++ freelancing_cli/src/command/stop.js
@@ -1,11 +1,17 @@
 const { showTimeEntries, stopTimeEntry } = require("../utils/saveTimeUtils");
-
+const chalk = require('chalk');
 const { getCurrentTime } = require("../utils/time");
 
 async function stopCommand(project) {
-  const stopTime = getCurrentTime();
-  await stopTimeEntry(project, stopTime);
-  console.log(`Stopped tracking time for ${project} at ${stopTime}`);
+  try {
+    const stopTime = getCurrentTime();
+    await stopTimeEntry(project, stopTime);
+    console.log(chalk.green(`Stopped tracking time for ${project} at ${stopTime}`));
+  } catch (error) {
+    console.error(chalk.red(`Error stopping time tracking for ${project}: ${error.message}`));
+    
+    throw error;
+  }
 }
 
 module.exports = { stopCommand };
