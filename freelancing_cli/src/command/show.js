a18303415305113b10810161132ebc9b7a46e096
Rahul Kumar <79074470+rahullkr@users.noreply.github.com>
2024-07-09 20:20:20 +0530

    Delete freelancing_cli directory

--- freelancing_cli/src/command/show.js
+++
@@ -1,8 +0,0 @@
-const { showTimeEntries } = require("../utils/saveTimeUtils");
-
-async function showCommand() {
-  const timeEntries = await showTimeEntries();
-  console.log("Time entries:", timeEntries);
-}
-
-module.exports = { showCommand };
